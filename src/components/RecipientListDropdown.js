56c3778355df35249a18a9d876f4abbaf3508fc5
invtfl-bharti <bhartijayprakash19@gmail.com>
2025-02-02 23:25:30 +0530

    ScheduledInput updated

--- src/components/RecipientListDropdown.js
+++ src/components/RecipientListDropdown.js
@@ -13,7 +13,8 @@ const RecipientListDropdown = ({
       <button
         onClick={() => setShowListDropdown(!showListDropdown)}
         className="w-full flex items-center justify-between p-3 border rounded-lg hover:bg-gray-50"
-      >
+          >
+              
         <div className="flex items-center gap-2">
           <Users className="w-5 h-5 text-gray-500" />
           <span>{recipientLists[selectedList].name}</span>
