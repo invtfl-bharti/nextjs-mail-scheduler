a499a22c5ebc3207dfd95238157c38e8b5afd6ba
invtfl-bharti <bhartijayprakash19@gmail.com>
2025-02-03 15:05:46 +0530

    Created the custom-hooks to manage dropdown menus

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 "use client";
-import Image from "next/image";
-import ScheduledInput from "../components/ScheduleInput";
-import ScheduledList from "../components/ScheduledList";
-import MailTemplate from "../components/MailTemplate";
+
+import ScheduleInput from "@/components/ScheduleInput";
+import MailTemplateEditor from "@/components/MailTemplate";
+import ScheduledList from "@/components/ScheduledList";
 
 export default function Home() {
   return (
-    <div className="bg-white h-full w-full p-10 px-40 flex flex-col text-black gap-4">
-      <div className="w-full h-2/3 flex justify-between items-center gap-4">
-        <ScheduledInput />
-        <MailTemplate />
-        
+    <div className="h-full w-full bg-white py-10 px-40 flex flex-col text-black gap-4">
+      <div className="w-full h-full flex justify-between items-center gap-4">
+        <ScheduleInput />
+        <MailTemplateEditor />
       </div>
-      <div className="h-full w-full flex items-center justify-center gap-4">
+      <div className="w-full h-full flex items-center justify-center">
         <ScheduledList />
       </div>
     </div>
